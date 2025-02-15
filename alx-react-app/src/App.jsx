77e1be3c1dcd2ce063908de3d2297b902a91323e
thabie05 <105032791+thabie05@users.noreply.github.com>
2025-02-15 13:56:33 +0200

    task  2

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header  from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -30,6 +33,9 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   )
 }
