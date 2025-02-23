635b373032db2acb94306c24675f8a5c72a867bd
Thabelo Tshishonga <thabelo.tshishonga@gmail.com>
2025-02-23 07:40:56 +0200

    counter added

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import UserProfile from './components/UserProfile'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import Counter from './components/Conter'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -39,6 +40,7 @@ function App() {
       age="25"
       bio="Loves hiking and photography"
        />
+       <Counter />
       <Header />
       <MainContent />
       <Footer />
