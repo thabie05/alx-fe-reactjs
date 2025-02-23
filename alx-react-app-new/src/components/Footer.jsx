ba970a218f13ead0166d72a9cd0fb4bf24d8d354
Thabelo Tshishonga <thabelo.tshishonga@gmail.com>
2025-02-23 07:14:21 +0200

    new

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return(
+        <footer style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+        <p>© 2023 City Lovers</p>
+      </footer>
+      );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return(
+    <header style={{border:"1px solid #459", backgroundColor:"#459"}}>
+    <h1>My Favorite Cities</h1>
+  </header>
+  );
+};
+
+export default Header;
