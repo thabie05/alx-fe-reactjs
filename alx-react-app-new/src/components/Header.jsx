d863001be6fccb6c602ec8659adc3c738feb61f5
Thabelo Tshishonga <thabelo.tshishonga@gmail.com>
2025-02-23 07:18:18 +0200

    push

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
     return(
-    <header style={{border:"1px solid #459", backgroundColor:"#459"}}>
+    <header style={{color: "#fff", textAlign: "center",border:"1px solid #459", backgroundColor:"#459"}}>
     <h1>My Favorite Cities</h1>
   </header>
   );
