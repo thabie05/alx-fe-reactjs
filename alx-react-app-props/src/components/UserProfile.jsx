5cea1fc26dfb5754dcacdc01ceadd08db45c6725
Thabelo Tshishonga <thabelo.tshishonga@gmail.com>
2025-02-24 06:11:44 +0200

    fix

---
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -0,0 +1,17 @@
+// UserProfile.jsx
+import React, { useContext } from 'react';
+import UserContext from './UserContext';
+
+function UserProfile() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserProfile;
