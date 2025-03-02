1a4d06e0c8f2ed365901e17db910bc8b16869163
thabie05 <105032791+thabie05@users.noreply.github.com>
2025-03-02 11:33:33 +0200

    ok new app

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,15 @@
+
+import AddRecipeForm from "./components/AddRecipeForm"
+import RecipeList from "./components/RecipeList"
+
+function App() {
+  
+  return (
+    <>
+      <AddRecipeForm />
+      <RecipeList />
+    </>
+  )
+}
+
+export default App
