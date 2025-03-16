65521d83279d3ed11576e69a2688db8a8158cd05
thabie05 <105032791+thabie05@users.noreply.github.com>
2025-03-16 11:20:56 +0200

    fixed text color on p

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -5,7 +5,7 @@ function UserProfile() {
       <div className="user-profile bg-gray-100 p-8 max-w-sm max-auto my-20 rounded-lg shadow-lg">
         <img className="rounded-full w-36 h-36 mx-auto" src="/JohnDoe.jpeg" alt="User" />
         <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-        <p className="text-blue-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+        <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
     );
   }
