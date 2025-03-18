ceb6a03a60adb621bdf1c7f71cb1324989b43981
Thabelo Tshishonga <thabelo.tshishonga@gmail.com>
2025-03-18 20:38:07 +0200

    style

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -13,10 +13,21 @@ const HomePage = () => {
 return(
     <div>
         {recipedata.map((recipe) => (
-            <div className="bg-blue-700 w-60 h-60 border-dotted rounded gap-3" key={recipe.id}>
-                <h1>{recipe.title}</h1>
+            <div className="
+            taxt-white
+            bg-green-600 
+            w-60 p-3 m-3 
+            bg-gradient-to-r 
+            from-red-500 h-50 
+            border-dotted 
+            rounded-xl 
+            transition delay-150 duration-300 
+            ease-in-out hover:-translate-y-1 
+            hover:scale-110" 
+            key={recipe.id}>
+                <h1 className="text-white ">{recipe.title}</h1>
                 <img src={recipe.image} alt={recipe.title} />
-                <p>{recipe.summary}</p>
+                <p className="text-white">{recipe.summary}</p>
             </div>
         ))}
     </div>
