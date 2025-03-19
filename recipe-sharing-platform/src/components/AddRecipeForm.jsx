244291d940d0b3aceae0b7c1f77ba85f17196f38
thabie05 <105032791+thabie05@users.noreply.github.com>
2025-03-19 14:38:00 +0200

    how

--- recipe-sharing-platform/src/components/AddRecipeForm.jsx
+++ recipe-sharing-platform/src/components/AddRecipeForm.jsx
@@ -1,4 +1,23 @@
+import { useState } from "react";
+
 const AddRecipeForm = () => {
+
+    const [title, setTitle] = useState('');
+    const [ingredients, setIngredients] = useState('');
+    const [instructions, setInstructions] = useState('');
+
+    const handleChange = (e) => {
+        if (e.target.name === 'title') {
+            setTitle(e.target.value);
+        } else if (e.target.name === 'ingredients') {
+            setIngredients(e.target.value);
+        } else {
+            setInstructions(e.target.value);
+        }
+    }
+
+
+
     const handleSubmit = (e) => {
         e.preventDefault();
         const title = e.target.elements.title.value;
@@ -6,14 +25,14 @@ const AddRecipeForm = () => {
         const instructions = e.target.elements.instructions.value;
         console.log(title, ingredients, instructions);
     }
-    
+
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <label className="text-gray-700 text-2xl font-bold mb-3 ">Recipe<input className="bg-gray-300 text-gray-700" type="text" placeholder="Title" name="title"/></label>
-                <label>ingredients<textarea placeholder="ingredients" name="ingredients"/></label>
-                <label>instructions<textarea placeholder="instructions" name="instructions"/></label>
+                <label className="text-gray-700 text-2xl font-bold mb-3 ">Recipe<input onChange={handleChange} className="bg-gray-300 text-gray-700" type="text" placeholder="Title" name="title"/></label>
+                <label>ingredients<textarea onChange={handleChange} placeholder="ingredients" name="ingredients"/></label>
+                <label>instructions<textarea onChange={handleChange} placeholder="instructions" name="instructions"/></label>
                 <button className="inline-block bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700 transition-colors duration-300" type="submit">Submit</button>
             </form>
         </div>
