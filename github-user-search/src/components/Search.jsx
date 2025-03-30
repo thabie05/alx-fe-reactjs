767204a04ad80f42f21e5ad5aa9c750fe41d5fc1
thabie05 <105032791+thabie05@users.noreply.github.com>
2025-03-30 11:47:56 +0200

    ok

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { fetchUserData } from '../../services/githubService';
+import { fetchUserData } from '../services/githubService';
 
 const Search = () => {
   const [username, setUsername] = useState('');
